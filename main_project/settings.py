14268d7fa1f465ac2a6a2f352777074be13c2d6e
vishal <vishal.mca20.du@gmail.com>
2022-02-24 15:19:55 +0530

    Added media in settings,.py

--- main_project/settings.py
+++ main_project/settings.py
@@ -145,6 +145,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_ROOT = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (

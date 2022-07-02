002b947936d1fe3ce75d11a1ae90009e59f8c0ea
Vishal Gupta <vishal.mca20.du@gmail.com>
2022-07-03 02:47:40 +0530

    Update settings.py

--- main_project/settings.py
+++ main_project/settings.py
@@ -178,12 +178,12 @@ LOGIN_URL = 'login'
 # CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = config('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
 
 
 # Configure Django App for Heroku.
